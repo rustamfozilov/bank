68d1810d624f73d445189cbc5903f547ee6603b6
rustam fozilov <rustamfozilov1998@gmail.com>
2021-09-12 22:08:20 +0300

    sad

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -1,1 +1,21 @@
 package stats
+
+import (
+	"fmt"
+	"github.com/rustamfozilov/bank/pkg/bank/types"
+)
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+			Amount: 100,
+		},
+		{
+			Amount: 200,
+		},
+	}
+
+	fmt.Println(Avg(payments))
+	// Output:
+	// 150
+}
